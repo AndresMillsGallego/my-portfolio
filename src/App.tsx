d544e63a4ce41a198f0082268fcb91fe5020ca9f
Andres Mills Gallego <geneseewoodworking@gmail.com>
2025-04-27 13:42:32 -0700

    Added the keyboard component as well as the individual keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import style from "./App.module.scss";
+import Splash from "./Pages/Splash/Splash";
 
 function App() {
-  return <div className={style["app"]}></div>;
+  return (
+    <div className={style["app"]}>
+      <Splash />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Keyboard/VolumeKnob/VolumeKnob.tsx
@@ -0,0 +1,7 @@
+import style from "./VolumeKnob.module.scss";
+
+const VolumeKnob = () => {
+  return <div className={style["volume-div"]}></div>;
+};
+
+export default VolumeKnob;
