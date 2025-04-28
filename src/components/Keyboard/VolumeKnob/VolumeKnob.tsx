18bb6881d8b0a149a1f12cdf77f5e972c38df04d
Andres Mills Gallego <geneseewoodworking@gmail.com>
2025-04-27 21:16:43 -0700

    Added the animation and MP3 audio track to the record volume knob.  Installed Motion and added a draggable interaction

--- src/components/Keyboard/VolumeKnob/VolumeKnob.tsx
+++ src/components/Keyboard/VolumeKnob/VolumeKnob.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useMemo, useState } from "react";
 import style from "./VolumeKnob.module.scss";
+import ClaireDeLune from "../../../assets/audio/ClairDeLune.mp3";
 
 const VolumeKnob = () => {
-  return <div className={style["volume-div"]}></div>;
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const song = useMemo(() => {
+    return new Audio(ClaireDeLune);
+  }, []);
+
+  const startRecord = isPlaying;
+
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+
+    if (isPlaying) {
+      song.pause();
+    }
+  };
+
+  useEffect(() => {
+    if (startRecord) {
+      song.play();
+    }
+  }, [song, startRecord]);
+
+  return (
+    <div
+      className={`${style["volume-div"]} ${
+        style[isPlaying ? "record-playing" : ""]
+      }`}
+      onClick={() => handleClick()}
+      title="Play me 🎶"
+    ></div>
+  );
 };
 
 export default VolumeKnob;
