8ff16c3738d57746a2a9ffdf0ee94e8610201202
Andres Mills Gallego <geneseewoodworking@gmail.com>
2025-05-02 14:48:02 -0700

    Added the CSS for the typewriter effect.  Updated drag contraints since the useRef was not working.  Added onClick to the code icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,30 @@
 import style from "./Header.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { AppContext } from "../../Context/context";
 
 const Header = () => {
+  const { setShowSplashPage, setShowPage } = useContext(AppContext);
+
+  const handleIconClick = () => {
+    setShowSplashPage(true);
+    setShowPage(false);
+  };
+
   return (
     <div className={style["header"]}>
       <div className={style["header-title"]}>
-        <FontAwesomeIcon icon={faCode} />
+        <FontAwesomeIcon
+          icon={faCode}
+          onClick={() => handleIconClick()}
+          className={style["code-icon"]}
+        />
         <div className={style["header-name"]}>
           <span>Andres Mills Gallego</span> <br />
-          <span>Software Engineer</span>
+          <div className={style["title"]}>
+            <span>Software Engineer</span>
+          </div>
         </div>
       </div>
     </div>
